881d2b6ae662196c20744fa44f2484cbf3b0de9c
Rin Ng <Rin08.ng@gmail.com>
2018-05-25 11:22:46 +0700

    Added instance method

--- models/menu.js
+++ models/menu.js
@@ -4,15 +4,20 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     price: DataTypes.INTEGER
   }, {
-    hooks: {
-      beforeCreate:(Menu, options) =>{
-        Menu.name = `${Menu.name} (Hot/Cold)`;
-      }
-    }
+    // hooks: {
+    //   beforeCreate:(Menu, options) =>{
+    //     Menu.name = `${Menu.name} (Hot/Cold)`;
+    //   }
+    // }
   });
   Menu.associate = function(models) {
     Menu.belongsToMany(models.Ingredient, { through: models.Recipe })
     Menu.hasMany(models.Order);
   };
+
+  Menu.prototype.getType = function(){
+    return (this.name + " (Hot/Cold)");
+  }
+  
   return Menu;
 };
